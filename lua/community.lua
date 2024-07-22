2b1135c47d1e4acd2ad559c235b063b9ae34c9aa
tim-huang <tim.huang@foxmail.com>
2024-07-22 23:26:34 +0800

    neovide支持/增加了一些社区插件

--- lua/community.lua
+++ lua/community.lua
@@ -1,4 +1,3 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
 
 -- AstroCommunity: import any community modules here
 -- We import this file in `lazy_setup.lua` before the `plugins/` folder.
@@ -9,4 +8,57 @@ return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.pack.lua" },
   -- import/override with your plugins folder
+
+  { import = "astrocommunity.pack.go" },
+  { import = "astrocommunity.pack.html-css" },
+  { import = "astrocommunity.pack.markdown" },
+  { import = "astrocommunity.pack.python" },
+  { import = "astrocommunity.pack.rust" },
+  { import = "astrocommunity.pack.tailwindcss" },
+  { import = "astrocommunity.pack.typescript" },
+  { import = "astrocommunity.pack.vue" },
+  { import = "astrocommunity.pack.json" },
+  { import = "astrocommunity.pack.yaml" },
+  { import = "astrocommunity.pack.cmake" },
+  { import = "astrocommunity.pack.cpp" },
+
+  -- editing
+  { import = "astrocommunity.editing-support.refactoring-nvim" },
+  -- { import = "astrocommunity.editing-support.rainbow-delimiters-nvim" },
+  { import = "astrocommunity.editing-support.neogen" },
+  { import = "astrocommunity.editing-support.nvim-devdocs" },
+  { import = "astrocommunity.editing-support.mini-splitjoin" },
+  { import = "astrocommunity.editing-support.vim-move" },
+  { import = "astrocommunity.editing-support.wildfire-nvim" },
+
+  -- markdown and latex
+  -- { import = "astrocommunity.markdown-and-latex.markdown-preview-nvim" },
+
+  -- marks
+  { import = "astrocommunity.motion.marks-nvim" },
+
+  -- diagnostics
+  { import = "astrocommunity.diagnostics.trouble-nvim" },
+
+  -- debugging
+  { import = "astrocommunity.debugging.persistent-breakpoints-nvim" },
+  { import = "astrocommunity.debugging.nvim-dap-virtual-text" },
+
+  -- testing
+  { import = "astrocommunity.test.neotest" },
+  { import = "astrocommunity.test.nvim-coverage" },
+
+  -- utility
+  -- there is an issue in transparent-vim
+  { import = "astrocommunity.utility.noice-nvim" },
+
+  -- colorscheme
+  { import = "astrocommunity.colorscheme.catppuccin" },
+  { import = "astrocommunity.colorscheme.onedarkpro-nvim" },
+  { import = "astrocommunity.colorscheme.github-nvim-theme" },
+
+  -- color
+  -- { import = "astrocommunity.color.transparent-nvim" },
+
+  -- { import = "astrocommunity.completion.copilot-lua-cmp" },
 }
---
+++ lua/plugins/neovide.lua
@@ -0,0 +1,73 @@
+-- Configuration for neovide
+-- For further information, visit https://neovide.dev/configuration.html
+if vim.g.neovide then
+  -- Font configuration, format : Primary\ Font,Fallback\ Font\ 1,Fallback\ Font\ 2:option1:option2:option3
+  vim.o.guifont = "JetBrainsMono Nerd Font:h12"
+
+  -- Background color and transparency
+  -- local alpha = function() return string.format("%x", math.floor((255 * vim.g.neovide_transparency_point) or 0.8)) end
+  vim.g.neovide_transparency = 0.8
+  -- vim.g.transparency = 0.0
+  -- vim.g.neovide_window_blurred = true
+  -- vim.g.neovide_background_color = "#0f1117" .. alpha()
+  -- Add keybinds to change transparency
+  local change_transparency = function(delta)
+    vim.g.neovide_transparency = vim.g.neovide_transparency + delta
+    -- vim.g.neovide_background_color = "#0f1117" .. alpha()
+  end
+  vim.keymap.set({ "n", "v", "o" }, "<D-]>", function() change_transparency(0.01) end)
+  vim.keymap.set({ "n", "v", "o" }, "<D-[>", function() change_transparency(-0.01) end)
+
+  -- Option key as meta in macOS
+  vim.g.neovide_input_macos_option_key_is_meta = "both"
+
+  -- Animate switch to command line
+  vim.g.neovide_cursor_animate_command_line = true
+
+  -- Cursor particles
+  vim.g.neovide_cursor_vfx_mode = "railgun"
+  vim.g.neovide_cursor_vfx_particle_lifetime = 1.5
+  vim.g.neovide_cursor_vfx_particle_density = 10.0
+
+  -- Hide mouse
+  vim.g.neovide_hide_mouse_when_typing = true
+
+  -- Only enable IME in insert & search mode
+  local function set_ime(args)
+    if args.event:match "Enter$" then
+      vim.g.neovide_input_ime = true
+    else
+      vim.g.neovide_input_ime = false
+    end
+  end
+
+  -- IME is enabled only in insert/command line mode
+  local ime_input = vim.api.nvim_create_augroup("ime_input", { clear = true })
+
+  vim.api.nvim_create_autocmd({ "InsertEnter", "InsertLeave" }, {
+    group = ime_input,
+    pattern = "*",
+    callback = set_ime,
+  })
+
+  vim.api.nvim_create_autocmd({ "CmdlineEnter", "CmdlineLeave" }, {
+    group = ime_input,
+    pattern = "[/\\?]",
+    callback = set_ime,
+  })
+
+  -- Enable cmd+c/cmd+v/cmd+s
+  vim.keymap.set("n", "<D-s>", ":w<CR>") -- Save
+  vim.keymap.set("v", "<D-c>", '"+y') -- Copy
+  vim.keymap.set("n", "<D-v>", '"+P') -- Paste normal mode
+  vim.keymap.set("v", "<D-v>", '"+P') -- Paste visual mode
+  vim.keymap.set("c", "<D-v>", "<C-R>+") -- Paste command mode
+  vim.keymap.set("i", "<D-v>", '<ESC>l"+Pli') -- Paste insert mode
+
+  -- Toggle fullscreen
+  local toggleFullScreen = function() vim.g.neovide_fullscreen = not vim.g.neovide_fullscreen end
+  vim.keymap.set("", "<D-f>", toggleFullScreen)
+  vim.keymap.set("", "<C-D-f>", toggleFullScreen)
+end
+
+return {}
